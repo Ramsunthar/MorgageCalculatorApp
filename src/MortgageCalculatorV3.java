711f87b6f363eafede15074356a4c7b76b19f766
Ramsunthar <ramsunthar.s@gmail.com>
2025-10-17 00:05:57 +0100

    refactored the code - repetitive pattern

--- src/MortgageCalculatorV3.java
+++ src/MortgageCalculatorV3.java
@@ -1,46 +1,16 @@
 import java.text.NumberFormat;
 import java.util.Scanner;
-//CLean Code
+/*
+* CLEAN CODE
+* 1. extracting method and make it a reusable unit
+* 2. refactoring the code by finding repetitive patterns in the whole code and make it is a template function.
+* */
 public class MortgageCalculatorV3 {
     public static void main(String[] args) {
 
-        int principal = 0;
-        byte years = 0;
-        float annualInterest =0;
-
-        Scanner scanner = new Scanner(System.in);
-
-        while(true) {
-            System.out.print("Principal ($1K - $1M): ");
-            principal = scanner.nextInt();
-            if(principal>=1000 && principal <= 1_000_000) {
-                break;
-            }
-            else {
-                System.out.println("Enter the value greater or equal to 1K and less than or equal to 1M");
-            }
-        }
-
-        while(true) {
-            System.out.print("Annual Interest Rate: ");
-            annualInterest = scanner.nextFloat();
-            if(annualInterest>0 && annualInterest <= 30)
-                break;
-            else {
-                System.out.println("Enter the Annual Interest value greater 0 and less than or equal to 30");
-            }
-        }
-
-        while(true) {
-            System.out.print("Period (Years) :");
-            years = scanner.nextByte();
-            if(years >=0 && years <= 30)
-                break;
-            else {
-                System.out.println("Enter the years greater 0 and less than or equal to 30");
-            }
-
-        }
+        int principal = (int)readNumber("Principal ($1K - $1M)", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate", 0, 30);
+        short years = (short) readNumber("Period (Years)",0,30);
 
         double mortgage = calculateMortgage(principal, annualInterest, years);
 
@@ -48,12 +18,29 @@ public class MortgageCalculatorV3 {
         System.out.println("Mortgage: " + mortgageFormatted);
     }
 
+    //Reading input and check the value  - common pattern function
+    public static double readNumber (String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while(true) {
+            System.out.print(prompt+": ");
+            value = scanner.nextDouble();
+            if(value>=min && value <= max) {
+                break;
+            }
+            else {
+                System.out.println("Enter the value between" + min + "and "+ max);
+            }
+        }
+        return value;
+    }
+
     //Mortgage Calculating function - making it easy for reusable
-    public static double calculateMortgage(int principal, float annualInterest, byte years) {
+    public static double calculateMortgage(int principal, float annualInterest, short years) {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        byte numberOfPayments = (byte)(years * MONTHS_IN_YEAR);
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
         float monthlyInterest = annualInterest/ PERCENT/ MONTHS_IN_YEAR;
 
 
