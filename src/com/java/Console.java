b5abbfb89c5df9ff6b5595e7182f44621f86faeb
Ramsunthar <ramsunthar.s@gmail.com>
2025-12-30 21:37:51 +0000

    overloaded the readNumber method-Concole class

--- src/com/java/Console.java
+++ src/com/java/Console.java
@@ -7,9 +7,16 @@ import java.util.Scanner;
  * This does one job, which is to read user input. so it has nothing to do with the other part of the code in the main class
  **/
 public class Console {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    //in case of getting inputs without range. - Overloaded method
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
     //Reading input and check the value  - common pattern function
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt + ": ");
