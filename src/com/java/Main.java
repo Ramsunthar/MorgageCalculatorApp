4e956fcc54c56e0d3041f0cc1d29a1d1874b4d3e
Ramsunthar <ramsunthar.s@gmail.com>
2026-01-03 21:45:36 +0000

    Inhertiance started

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -2,17 +2,23 @@ package com.java;
 
 public class Main {
 
-    //5 lines of main method
+    //Main method of the mortgage Calculator
+//    public static void main(String[] args) {
+//        int principal = (int) Console.readNumber("Principal ($1K - $1M)", 1000, 1_000_000);
+//        float annualInterest = (float) Console.readNumber("Annual Interest Rate", 0, 30);
+//        byte years = (byte) Console.readNumber("Period (Years)",0,30);
+//
+//        var calculator = new MortgageCalculator(principal, annualInterest, years);
+//
+//        var report = new MortgageReport(calculator);
+//        report.printMortgage();
+//        report.printPaymentSchedule();
+//    }
+
     public static void main(String[] args) {
-        int principal = (int) Console.readNumber("Principal ($1K - $1M)", 1000, 1_000_000);
-        float annualInterest = (float) Console.readNumber("Annual Interest Rate", 0, 30);
-        byte years = (byte) Console.readNumber("Period (Years)",0,30);
-
-        var calculator = new MortgageCalculator(principal, annualInterest, years);
-
-        var report = new MortgageReport(calculator);
-        report.printMortgage();
-        report.printPaymentSchedule();
+        var control = new Textbox();
+        control.disable();
+        System.out.println(control.isEnabled());
     }
 
 
---
+++ src/com/java/Textbox.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Textbox extends UIControl{
+    private String text = "";
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
